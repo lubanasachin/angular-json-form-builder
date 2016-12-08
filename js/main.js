37bbd911c17a7d2bc4169e63d87cffed22e57bb1
Sachin Lubana <sachin.lubana@bookmyshow.com>
2016-12-09 01:43:54 +0530

    changes made in form builder to handle form navigations while designing processes

--- js/main.js
+++ js/main.js
@@ -1,186 +1,153 @@
-var fieldId = 0;
-var jsonData = {};
+var fieldId = 0,
+	formId = 1,
+	formJson = {}
+	resultJson = {};
 (function(angular) {
   'use strict';
 
 	angular.module('myProcessApp', [])
   	.controller('myProcessCtrl', ['$scope','$compile', '$rootScope',  function($scope,$compile, $rootScope) {
+
+		$scope.jsonView = true;
+		$scope.formView = false;
+		$scope.previousBtn = false;
+
 		$scope.addComponent = function(addMe) {
-            var settDiv = document.getElementById('settingArea');
-            var pelem = '';
-            switch(addMe) {
-                case 1:
-                    pelem = angular.element(document.createElement('my-textfield'));
-                    break;
-                case 2:
-                    pelem = angular.element(document.createElement('my-email'));
-                    break;
-                case 3:
-                    pelem = angular.element(document.createElement('my-radio'));
-                    break;
-                case 4:
-                    pelem = angular.element(document.createElement('my-checkbox'));
-                    break;
-                case 5:
-                    pelem = angular.element(document.createElement('my-label'));
-                    break;
-                case 6:
-                    pelem = angular.element(document.createElement('my-button'));
-                    break;
-            }
-            settDiv.append($compile(pelem)($scope)[0]);
+			var formDiv = document.getElementById('form-'+formId),
+            	pelem = document.createElement('my-element');
+			pelem.setAttribute('element-type',addMe);
+			pelem = angular.element(pelem);
+            formDiv.append($compile(pelem)($scope)[0]);
 			fieldId++;
 		}
 
+		$scope.addForm = function() {
+			if(formId > 1) {
+				$scope.previousBtn = true;
+				document.getElementById('form-'+(formId-1)).style.display = 'none';
+			} else $scope.previousBtn = false;
+	
+			if(document.getElementById('form-'+formId) != undefined) {
+				document.getElementById('form-'+formId).style.display = 'block';
+				formJson = resultJson['form-'+formId] || {};
+				fieldId = Object.keys(formJson).length;
+			} else {
+				var settDiv = document.getElementById('settingArea'),
+					formDiv = document.createElement('div');
+				formDiv.setAttribute('id','form-'+formId);
+				settDiv.append(formDiv);	
+				formJson = {};
+				fieldId = 0;
+			}
+			document.getElementById("jsonArea").innerHTML = '<pre>'+JSON.stringify(formJson,undefined,2)+'</pre>';
+		}
+		$scope.addForm();
+
+		$scope.previousForm = function() {
+			document.getElementById('form-'+formId).style.display = 'none';
+			document.getElementById('form-'+(formId-1)).style.display = 'block';
+			formId--;
+			if(formId <= 1) $scope.previousBtn = false;
+			formJson = resultJson['form-'+formId];
+			fieldId = Object.keys(formJson).length;
+			document.getElementById("jsonArea").innerHTML = '<pre>'+JSON.stringify(formJson,undefined,2)+'</pre>';
+		}
+
+		$scope.saveMore = function() {
+			if(Object.keys(formJson).length <= 0) return;
+			resultJson['form-'+formId] = formJson;			
+			formId++;
+			$scope.addForm();
+		}
+
 		$scope.saveForm = function() {
-			console.log('SAVE FORM CLICKED');
+			if(Object.keys(formJson).length <= 0) return;
+			$scope.previousBtn = false;
+			resultJson['form-'+formId] = formJson;			
+			console.log(resultJson);
+            document.getElementById('settingArea').innerHTML = '';
+			resultJson = {};
+			formJson = {};
+			fieldId = 0;
+			document.getElementById("jsonArea").innerHTML = '<pre>'+JSON.stringify(formJson,undefined,2)+'</pre>';
+		}
+
+		$scope.viewForm = function() {
+			$scope.jsonView = false;
+			$scope.formView = true;
+		}
+
+		$scope.viewJson = function() {
+			$scope.formView = false;
+			$scope.jsonView = true;
 		}
 
 		$scope.resetForm = function() {
-            document.getElementById('settingArea').innerHTML = '';
+            document.getElementById('form-'+formId).innerHTML = '';
+			formJson = {};
+			fieldId = 0;
+			document.getElementById("jsonArea").innerHTML = '<pre>'+JSON.stringify(formJson,undefined,2)+'</pre>';
 		}
 
   	}])
-	.directive('myTextfield', function() {
-		return {
-			restrict: 'E',
-			transclude: true,
-            scope: true,
-            link: function($scope, element,attrs) {
-				$scope.field = {
-					fieldId: fieldId,
-					fieldName: 'New field '+fieldId,
-					defaultValue: '',
-					isRequired: false
-				};
-				
-                $scope.removeMe = function() {
-					delete jsonData["field-"+$scope.field.fieldId];
-					$scope.field = {};
-                    element.remove();
-					document.getElementById("jsonArea").innerHTML = '<pre>'+JSON.stringify(jsonData,undefined,2)+'</pre>';
-                }
-
-				$scope.$watch('field', function(newVal,oldVal) {
-					if($scope.field.fieldId === undefined) return;
-					jsonData["field-"+newVal.fieldId] = {
-						"field_title": newVal.fieldName,
-						"field_type": 'text',
-						"field_value": newVal.defaultValue,
-						"field_required": newVal.isRequired
-					}
-					document.getElementById("jsonArea").innerHTML = '<pre>'+JSON.stringify(jsonData,undefined,2)+'</pre>';
-				},true);
-
-            },
-			templateUrl: 'views/directives-templates/my-textfield.html'
-		}
-	})
-	.directive('myEmail', function() {
+	.directive('myElement', function() {
         return {
             restrict: 'E',
             transclude: true,
             scope: true,
             link: function($scope, element,attrs) {
+
                 $scope.field = {
                     fieldId: fieldId,
                     fieldName: 'New field '+fieldId,
+					fieldType: attrs.elementType,
                     defaultValue: '',
                     isRequired: false
                 };
 
+				if(attrs.elementType === 'image' && $scope.field.defaultValue === '') {
+					$scope.field.defaultValue = 'http://image.com/bg.png';
+				}
+
                 $scope.removeMe = function() {
-					delete jsonData["field-"+$scope.field.fieldId];
+                    delete formJson["field-"+$scope.field.fieldId];
                     $scope.field = {};
                     element.remove();
-                    document.getElementById("jsonArea").innerHTML = '<pre>'+JSON.stringify(jsonData,undefined,2)+'</pre>';
+                    document.getElementById("jsonArea").innerHTML = '<pre>'+JSON.stringify(formJson,undefined,2)+'</pre>';
+                }
+
+                $scope.addOptions = function() {
+                    var pElem = document.getElementById('optionsList');
+                    var box = document.createElement('div');
+                        box.className = "box";
+                    var txt = document.createElement('input');
+                        txt.className = "inputTxt";
+                    var close = document.createElement('label');
+                        close.className = "lbl";
+                        close.innerHTML = "&nbsp;X";
+                        close.addEventListener('click', function() {
+                            pElem.removeChild(box);
+                        });
+                    box.append(txt);
+                    box.append(close);
+                    pElem.append(box);
                 }
 
                 $scope.$watch('field', function(newVal,oldVal) {
                     if($scope.field.fieldId === undefined) return;
-                    jsonData["field-"+newVal.fieldId] = {
+                    formJson["field-"+newVal.fieldId] = {
                         "field_title": newVal.fieldName,
-                        "field_type": 'email',
+                        "field_type": newVal.fieldType,
                         "field_value": newVal.defaultValue,
                         "field_required": newVal.isRequired
                     }
-                    document.getElementById("jsonArea").innerHTML = '<pre>'+JSON.stringify(jsonData,undefined,2)+'</pre>';
+                    document.getElementById("jsonArea").innerHTML = '<pre>'+JSON.stringify(formJson,undefined,2)+'</pre>';
                 },true);
 
             },
-            templateUrl: 'views/directives-templates/my-email.html'
-        }
-    })
-    .directive('myRadio', function() {
-        return {
-            restrict: 'E',
-            transclude: true,
-            scope: true,
-            link: function($scope, element,attrs) {
-                $scope.fieldName = 'New field '+fieldId;
-                $scope.removeMe = function() {
-                    element.remove();
-                }
-				$scope.addOptions = function() {
-					var pElem = document.getElementById('optionsList');
-					var box = document.createElement('div');
-						box.className = "box";
-					var txt = document.createElement('input');
-						txt.className = "inputTxt";
-					var close = document.createElement('label');
-						close.className = "lbl";
-						close.innerHTML = "&nbsp;X";
-						close.addEventListener('click', function() {
-							pElem.removeChild(box);
-						});
-					box.append(txt);
-					box.append(close);
-					pElem.append(box);
-				}
-            },
-            templateUrl: 'views/directives-templates/my-radio.html'
-        }
-    })
-    .directive('myCheckbox', function() {
-        return {
-            restrict: 'E',
-            transclude: true,
-            scope: true,
-            link: function($scope, element,attrs) {
-                $scope.fieldName = 'New field '+fieldId;
-                $scope.removeMe = function() {
-                    element.remove();
-                }
-            },
-            templateUrl: 'views/directives-templates/my-checkbox.html'
-        }
-    })
-    .directive('myLabel', function() {
-        return {
-            restrict: 'E',
-            transclude: true,
-            scope: true,
-            link: function($scope, element,attrs) {
-                $scope.fieldName = 'New field '+fieldId;
-                $scope.removeMe = function() {
-                    element.remove();
-                }
-            },
-            templateUrl: 'views/directives-templates/my-label.html'
-        }
-    })
-    .directive('myButton', function() {
-        return {
-            restrict: 'E',
-            transclude: true,
-            scope: true,
-            link: function($scope, element,attrs) {
-                $scope.fieldName = 'New field '+fieldId;
-                $scope.removeMe = function() {
-                    element.remove();
-                }
-            },
-            templateUrl: 'views/directives-templates/my-button.html'
+            templateUrl: function(element,attrs) {
+				return 'views/directives-templates/my-'+attrs.elementType+'.html';
+			}
         }
     })
 })(window.angular);
